ce409cfb8c8e84534b7a312eaf30c450958eb4d2
holytrees369 <futian_yongtao@163.com>
2021-02-24 13:41:35 +0800

    add:提交javaee联系代码

---
+++ Java/src/basic/variables/ClassVariables.java
@@ -0,0 +1,18 @@
+package basic.variables;
+
+/**
+ * @author YongTao
+ * @since 2021-02-05 15 : 02
+ *
+ * 类变量（静态变量）
+ */
+public class ClassVariables {
+
+    /*
+        用static 声明
+        
+
+     */
+
+
+}
